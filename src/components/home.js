872164babc1698d32a1927032b21e27abfd6fdef
Cindy Nguyen <cnguyen0@uw.edu>
2018-06-10 02:09:10 -0700

    fixed webpack config

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react'
 import { Image, Grid } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
 
 import Portfolio from './portfolio.jsx'
 import Creative from './creative.jsx'
